2b2d3f0701b141bcb3d4ea13b0f074ce8cd026b0
RichardAngka <angkawinata25@gmail.com>
2022-05-05 19:11:56 +0700

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Richard Angkawinata on 05/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Richard Angkawinata on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
